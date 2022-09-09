8a832f9e9c6856b717f716de5d70825fe259bba5
fgoettel <fabian.goettel@gmail.com>
2022-09-10 00:11:32 +0200

    Slim down example.

--- logger/examples/main.py
+++ logger/examples/main.py
@@ -6,8 +6,6 @@ import asyncio
 import logging
 from pathlib import Path
 
-from xknx.io import ConnectionType
-
 from logger.runner import run as logger_run
 
 
@@ -25,14 +23,8 @@ def main() -> int:
             db_addr=addr,
             knx_mapping=mapping,
             knx_own_address="1.1.99",
-            knx_gateway_ip="10.1.1.104",
-            knx_gateway_port=3671,
             knx_route_back=False,
-            knx_local_ip="10.1.1.135",
             knx_local_port=3671,
-            knx_connection_type=ConnectionType.ROUTING,
-            status_server=False,
-            status_server_port=8080,
         )
     )
 
