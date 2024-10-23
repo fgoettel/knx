87cb10c2588a74b34d7b3e0feb43dea31e899619
fgoettel <fabian.goettel@gmail.com>
2024-10-23 21:45:54 +0200

    Remove 'safety' as this is now handled by github dependabot.

--- logger/noxfile.py
+++ logger/noxfile.py
@@ -9,7 +9,7 @@ from nox_poetry import session
 locations = ("examples", "logger", "test", "./noxfile.py")
 python_default = "3.12"
 python_versions = (python_default,)
-nox.options.sessions = "lint", "tests", "mypy", "safety"
+nox.options.sessions = "lint", "tests", "mypy"
 
 
 def open_in_browser(path: str) -> None:
@@ -55,20 +55,3 @@ def mypy(session: session) -> None:
     """Types, types, types."""
     session.install("mypy", "xknx", "pytest", "sqlalchemy-stubs")
     session.run("mypy", ".", *session.posargs)
-
-
-@session(python=python_versions)
-def safety(session: session) -> None:
-    """Check for security issues."""
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install("safety")
-        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
